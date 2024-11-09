d4638598b502dc76c7128e0cd6e17a243679ff1c
Simranjeet <simran7009191049@gmail.com>
2024-11-10 02:31:30 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
         
         // Send login request to the backend API
         try {
-            const response = await fetch('http://localhost:4000/dealsdray/login', {
+            const response = await fetch('https://backend-8nriwtfsk-simran-code-48s-projects.vercel.app/dealsdray/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
